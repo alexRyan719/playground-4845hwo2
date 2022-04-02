8ccc94c0a2e45c373fc8920258fcc17b34d3405c
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-04-02 12:17:25 -0600

    Create less_simple_switch.java

---
+++ less_simple_switch.java
@@ -0,0 +1,57 @@
+public class lessSimpleSwitch {
+
+	public static String returnMonth(int numMonth) {
+		
+		String month = "";
+		
+		switch(numMonth) {
+			case(1):
+				month = "January";
+				break;
+			case(2):
+				month = "February";
+				break;
+			case(3):
+				month = "March";
+				break;
+			case(4):
+				month = "April";
+				break;
+			case(5):
+				month = "May";
+				break;
+			case(6):
+				month = "June";
+				break;
+			case(7):
+				month += "July";
+				break;
+			case(8):
+				month += "August";
+				break;
+			case(9):
+				month = "September";
+				break;
+			case(10):
+				month = "October";
+				break;
+			case(11):
+				month = "November";
+				break;
+			case(12):
+				month = "December";
+				break;
+		
+		}
+		
+		return month;
+	}
+	
+	public static void main(String[] args) {
+		String test1 = new String(returnMonth(7));
+		System.out.println(test1);
+		
+
+	}
+
+}
